014ca3280faf02500312ec9cbc22088a0abadbb7
fengli <954751186@qq.com>
2019-04-15 14:57:13 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import "common/stylus/variable.styl";
+  #app
+    color: $color-sub-theme
+</style>
